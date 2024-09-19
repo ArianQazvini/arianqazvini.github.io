d32aa35540f7481321a8f1f62aaec8117844668f
ArianQazvini <a.qazvini@aut.ac.ir>
2024-09-19 12:50:24 +0330

    minor edits

--- script.js
+++ script.js
@@ -1,37 +1,34 @@
-// Function to toggle the visibility of the navbar
+
 function toggleNavbar() {
     const navbar = document.getElementById("navbar");
+    const menuIcon = document.getElementById("menuIcon");
+
     navbar.classList.toggle("navbar-visible");
+
+
 }
 
-// Function to hide the navbar when a link is clicked
 function hideNavbar() {
     const navbar = document.getElementById("navbar");
-    navbar.classList.remove("navbar-visible"); // Remove the class to hide the navbar
+    navbar.classList.remove("navbar-visible");
 }
-
-// Add click event listener to each menu item link to hide the navbar
 document.addEventListener("DOMContentLoaded", () => {
     const menuLinks = document.querySelectorAll('.navbar ul li a');
     menuLinks.forEach(link => {
         link.addEventListener('click', hideNavbar);
     });
 });
-
-// Function to scroll to the top
 function scrollToTop() {
     window.scrollTo({
         top: 0,
-        behavior: 'smooth' // Smooth scrolling effect
+        behavior: 'smooth'
     });
 }
-
-// Function to toggle the visibility of the scroll-to-top button
 window.onscroll = function() {
     const scrollToTopBtn = document.getElementById("scrollToTopBtn");
     if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-        scrollToTopBtn.style.display = "block"; // Show button when scrolled down 300px
+        scrollToTopBtn.style.display = "block";
     } else {
-        scrollToTopBtn.style.display = "none"; // Hide button when at the top
+        scrollToTopBtn.style.display = "none";
     }
 };
